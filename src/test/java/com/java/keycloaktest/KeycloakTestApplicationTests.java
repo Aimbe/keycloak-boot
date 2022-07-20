43430be835139621f685cedd36ddf2725f288196
Jay Park(Jeongho Park) <qkr2435@gmail.com>
2022-07-20 23:51:12 +0900

    Update KeycloakTestApplicationTests.java
    test용 설정

--- src/test/java/com/java/keycloaktest/KeycloakTestApplicationTests.java
+++ src/test/java/com/java/keycloaktest/KeycloakTestApplicationTests.java
@@ -3,9 +3,11 @@ package com.java.keycloaktest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//키클락 테스트용 부트 환경
 @SpringBootTest
 class KeycloakTestApplicationTests {
 
+    String test = null;
     @Test
     void contextLoads() {
     }
